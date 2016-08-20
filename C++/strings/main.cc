7ce96ebe291901699eb4539c72c7c0cfdac2f03d
ykdu <ykdu@hotmail.com>
2016-08-20 22:18:15 +0800

    blankreplace

--- C++/strings/main.cc
+++ C++/strings/main.cc
@@ -1,8 +1,64 @@
 #include <cstdio>
 #include <cassert>
 #include <cstdlib>
+#include <string>
+#include <iostream>
+using namespace std;
 
 namespace dyk{
+
+    // length is the
+    char* c_replace_blank(char* str_ipt, int size)
+    {
+        // O(n)
+        if(str_ipt == NULL || size <= 0)
+            return NULL;
+
+        int origin_len = 0;
+        int blank_len = 0;
+
+        for(int i = 0; str_ipt[i] != '\0'; i++)
+        {
+            origin_len++;
+
+            if(str_ipt[i] == ' ')
+                blank_len++;
+        }
+
+        int extra_len = origin_len + blank_len * 2;
+
+        if(extra_len > size)
+            return NULL;
+
+        for(int i = origin_len, j = extra_len; i >= 0; i--)
+        {
+            if(str_ipt[i] == ' ')
+            {
+                str_ipt[j--] = '0';
+                str_ipt[j--] = '2';
+                str_ipt[j--] = '%';
+            } else {
+                str_ipt[j--] = str_ipt[i];
+            }
+        }
+
+        return str_ipt;
+    }
+
+    string& replace_blank(string& str_ipt)
+    {
+        // O(n^2)
+        string::size_type it;
+
+        while((it = str_ipt.find(' ')) != string::npos)  // string::npos
+        {
+            str_ipt.replace(it, 1, "%20");
+        }
+
+        return str_ipt;
+    }
+
+
     int strcmp(const char* lhs, const char* rhs)
     {
         assert((NULL != lhs) && (NULL != rhs));
@@ -47,14 +103,10 @@ namespace dyk{
 int main(int argc, char* argv[])
 {
     char des[7] = "Hello ";
-    char src[20] = "World!";
-    printf("%d\n", dyk::strcmp(des, src));
-    printf("%s - %s \n", des, src);
-    dyk::strcat(des, src);
-    printf("%s - %s \n", des, src);
-    des[10] = 'S';
-    printf("%s - %s \n", des, src);
-    dyk::strcpy(des, src);
-    printf("%s - %s \n", des, src);
+    char src[20] = " World ! ";
+    string str("hello World ! ");
+    dyk::replace_blank(str);
+    dyk::c_replace_blank(src, 20);
+    cout<<src<<endl;
     return 0;
 }
