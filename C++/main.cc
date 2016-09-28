3632adb1c68c42cb339239bc78505cb07756e787
ykdu@CGCL <ykdu@hotmail.com>
2016-09-28 21:13:36 +0800

    update

--- C++/main.cc
+++ C++/main.cc
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <future>
 #include <iostream>
+#include <set>
 #include <vector>
 
 int fib(int x = 1) {
@@ -44,21 +45,65 @@ void func() {
     std::cout << str << std::endl;
 }
 
-int main() {
-    int n, m;
-    std::cin >> n >> m;
-    srand(time(NULL));
-    std::vector<int> vec;
-    vec.push_back(0);
-    vec.push_back(n);
+int a[100];
+static int b[100];
 
-    for (int i = 0; i < m - 1; i++) {
-        vec.push_back(rand() % n);
-    }
-    std::sort(vec.begin(), vec.end());
-    for (int i = 1; i < vec.size(); i++) {
-        std::cout << vec[i] - vec[i - 1] << " ";
+void print_arr(int* p, int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << p[i] << " ";
     }
     std::cout << std::endl;
+}
+
+int main() {
+    int i = 10;
+    const int* pi = &i;
+
+    int c[10] = {0};
+    int* pd = new int[10];
+
+    std::vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::set<int> sarr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (auto it = sarr.begin(); it != sarr.end();) {
+        auto p = it;
+        it++;
+        std::cout << *p << " ";
+        if (*p == 5) {
+            sarr.erase(p);
+        }
+        std::cout << *p << std::endl;
+    }
+
+    for (auto i : c) {
+        std::cout << i << std::endl;
+    }
+
+    std::cout << (void*)&c << " " << (void*)&c[0] << std::endl;
+    /*
+    print_arr(b, 100);
+    print_arr(c, 100);
+    print_arr(pd, 100);
+
+    std::cout << *pi << std::endl;
+    i = 11;
+    std::cout << *pi << std::endl;
+
+        int n, m;
+        std::cin >> n >> m;
+        srand(time(NULL));
+        std::vector<int> vec;
+        vec.push_back(0);
+        vec.push_back(n);
+
+        for (int i = 0; i < m - 1; i++) {
+            vec.push_back(rand() % n);
+        }
+        std::sort(vec.begin(), vec.end());
+        for (int i = 1; i < vec.size(); i++) {
+            std::cout << vec[i] - vec[i - 1] << " ";
+        }
+        std::cout << std::endl;
+        */
     return 0;
 }
